a4f818a9a31c6dd0f953e501a8c4bc461f1310a2
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-07-20 10:42:13 -0300

    Javascript
    Iniciando do 0 os estudos em JS

--- aula11/ex008.js
+++
@@ -1,7 +0,0 @@
-var vel = 12
-if (vel > 60) {
-    console.log("Multado")
-} 
-else {
-    console.log(`A velocidade do seu carro é ${vel}km`)
-}
--- aula11/ex009.js
+++
@@ -1,6 +0,0 @@
-var pais = 'Basil'
-if (pais == 'Brasil') {
-    console.log("Br")
-} else {
-    console.log("estrangeiro")
-}
