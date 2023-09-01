9b83ea50ba6fb849bb672ea97fcf1616ab9d596f
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-09-01 10:45:44 -0300

    aula 06 - laço de repetição
    While com teste logico no inicio e com o teste logico no final

---
+++ aulasjs/aula13/ambiente.js
@@ -0,0 +1,18 @@
+var cont = 1
+do {
+    console.log(`Paao ${cont}`)
+    cont++  
+} while(cont <= 6)
+
+/*
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+var cont = 1
+while (cont <= 3) {
+    console.log(`Paao ${cont}`)
+    cont++
+}*/
