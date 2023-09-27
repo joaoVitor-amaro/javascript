6ba84edaee3469e6627c7ee5f45068b6a9bcd3a7
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-09-27 10:04:23 -0300

    Update script.js

--- exerciciosJS/ex021/script.js
+++ exerciciosJS/ex021/script.js
@@ -10,25 +10,27 @@ document.getElementById("btn_cont").addEventListener("click", function() {
         resultado.innerHTML = "Bora contar"
         //contagem decrescente//
         if (Number(contFim) < Number(contInicio)) { 
-            //passo -1 caso o incremento seja maior ou igual a 0 
             if (Number(contIncremente) >= 0) {
+                //passo -1 caso o incremento seja maior ou igual a 0 
                 alert("Passo inválido! Considerando PASSO -1")
                 for (var i = Number(contInicio); i >= Number(contFim); i--) {
                     resultado.innerHTML += ` ${i} =>`
                 }
-            } else { //Inicia a contagem decrescente se o passor for menor que 0//
+            } else { 
+                //Inicia a contagem decrescente se o passor for menor que 0//
                 for (var i = Number(contInicio); i >= Number(contFim); i+=Number(contIncremente)) {
                     resultado.innerHTML += ` ${i} =>`
                 }
             }
         } else {  //contagem crescente//
-            //passo 1, caso o incremento seja igual a 0
             if (Number(contIncremente) === 0) { 
+                //passo 1, caso o incremento seja igual a 0
                 alert("Passo inválido! Considerando PASSO 1")
                 for (var i = Number(contInicio); i <= Number(contFim); i++) {
                     resultado.innerHTML += ` ${i} =>`
                 }
-            } else { //Inicia a contagem crecente se o passor for maior que 0//
+            } else {
+                 //Inicia a contagem crecente
                 for (var i = Number(contInicio); i <= Number(contFim); i += Number(contIncremente)) {
                     resultado.innerHTML += ` ${i} =>`
                 }
