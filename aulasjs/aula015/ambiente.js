285aa9c35ceee535367e9f9d93bdb946ff30f8f7
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-09-28 10:52:19 -0300

    aula 15
    aula sobre vetores, funções sobre vetores e como interar os vetores

---
+++ aulasjs/aula015/ambiente.js
@@ -0,0 +1,13 @@
+var itens = [5, 8, 2, 9, 3]
+/*itens.forEach(function(valores) {
+  console.log(valores)
+})
+console.log("----")
+for (var i = 0; i < itens.length; i++) {
+  console.log(itens[i])
+}
+console.log("----")
+for (var it in itens) {
+  console.log(itens[it])
+}*/
+console.log(itens.indexOf(1))
