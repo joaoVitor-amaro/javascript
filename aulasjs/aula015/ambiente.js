77ea3116de4c8a910c4296bb0a24632b3a528897
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-10-15 10:16:24 -0300

    aula 015
    função emetodos de vetor

--- aulasjs/aula015/ambiente.js
+++ aulasjs/aula015/ambiente.js
@@ -1,13 +1,21 @@
 var itens = [5, 8, 2, 9, 3]
-/*itens.forEach(function(valores) {
-  console.log(valores)
+itens.forEach(function(valores, id) {
+  console.log(valores, id)
 })
 console.log("----")
-for (var i = 0; i < itens.length; i++) {
-  console.log(itens[i])
+for (let valores of itens) {
+    console.log(valores)
 }
+
 console.log("----")
 for (var it in itens) {
   console.log(itens[it])
-}*/
+}
+console.log("----")
+console.log(itens.push(1))
 console.log(itens.indexOf(1))
+console.log(itens.shift())
+console.log(itens.unshift(2))
+console.log(itens)
+console.log(itens.splice(0,2))
+console.log(itens)
